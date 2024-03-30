cfb7c87df94e2fbffd04d29a44032e3b1125174c
safna2002 <safnafathima033@gmail.com>
2024-03-30 23:31:55 +0530

    assignment 3

---
+++ db.py
@@ -0,0 +1,7 @@
+district_list = [
+    {'id': 1, 'name': 'Kannur'},
+    {'id': 2, 'name': 'Malappuram'},
+    {'id': 3, 'name': 'Kollam'},
+    {'id': 4, 'name': 'Alappuzha'},
+    {'id': 5, 'name': 'Kottayam'}
+]

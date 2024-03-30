3f12062d03d05bf2c4a56093990e7da1dab085cc
safna2002 <safnafathima033@gmail.com>
2024-03-30 23:26:02 +0530

    assignment 3

---
+++ lrgsml.py
@@ -0,0 +1,8 @@
+def min_max(nums):
+    return min(nums), max(nums)
+
+# Example usage:
+input_list = [45, 56, 98, 3, 67, 9, 90]
+smallest, largest = min_max(input_list)
+
+print(f"Smallest number: {smallest}, Largest number: {largest}")
